a82080184b95d4ecdf12ca606a88729dc64ae870
Morten Warncke-Wang <mwang@wikimedia.org>
2022-07-01 16:21:33 +0000

    Update commons search aggregatio notebooks and cron job
    Minor updates to the Special:MediaSearch aggregation notebooks.
    Also updates the shell script to run all three aggregations
    thus only requiring the single script to run.

--- aggregate_commons_searches.sh
+++ aggregate_commons_searches.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 SE_PATH=/home/nettrom/src/2020-media-search
-PYTHON_HOME=/home/nettrom/venv/bin
+CONDA_ENV_NAME="sd_dashboards"
 
 cd $SE_PATH
 {
   date
 
   echo "Running daily aggregation of search on Commons"
-  $PYTHON_HOME/jupyter nbconvert --ExecutePreprocessor.timeout=1800 \
+
+  source conda-activate-stacked $CONDA_ENV_NAME
+  
+  jupyter nbconvert --ExecutePreprocessor.timeout=1800 \
        --to html --execute T258229_commons_search_aggregation.ipynb
-  hdfs dfs -chmod -R o+r /user/hive/warehouse/nettrom_sd.db/commons_search_counts
+  jupyter nbconvert --ExecutePreprocessor.timeout=1800 \
+       --to html --execute T258229_mediasearch_success_aggregation.ipynb
+  jupyter nbconvert --ExecutePreprocessor.timeout=1800 \
+       --to html --execute T258229_mediasearch_filter_usage.ipynb
+  hdfs dfs -chmod -R o+rx /user/hive/warehouse/nettrom_sd.db
 } >> $SE_PATH/commons_search_aggregation.log 2>&1
